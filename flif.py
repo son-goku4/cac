0aeafac9edb3171c6b55454f537ff2eb2078a031
son-goku4 <michaeldianov1@gmail.com>
2024-12-26 17:10:36 +0100

    Create flif.py

---
+++ flif.py
@@ -0,0 +1,18 @@
+import random
+import telbot 
+
+# замени 'TOKEN' на токен твоего бота
+# этот токен ты получаешь от botfather,чтобы бот мог работать
+bot = telebot.telebot("8166355541:aafbpbmtndvve3xbakm5wcfanvjffp4jipvm")
+
+@bot.massage_handler(commands=['start'])
+def send_welcome(massage):
+  bot.reply_to(massage,)
+
+@bot.massage_handler(commands=['start'])
+def send_hello(massage):
+  bot.reply_to(massage,)
+
+@bot.massage_handler(commands=['start'])
+def send_bye(massage):
+  bot.reply_to(massage,)
